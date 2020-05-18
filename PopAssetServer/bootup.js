785357bbf85e0c0b05d75d8fe696570dd18198b1
Graham Reeves <graham@grahamreeves.com>
2020-05-18 12:18:21 +0100

    Implementation wip

--- PopAssetServer/bootup.js
+++ PopAssetServer/bootup.js
@@ -4,42 +4,38 @@ Pop.Include = function(Filename)
 	return Pop.CompileAndRun( Source, Filename );
 }
 
-Pop.Include('PromiseQueue.js');
+Pop.Include('PopEngineCommon/PopApi.js');
 Pop.Include('PopEngineCommon/PopAssetServer.js');
 
+
+
+
+function RedirectDebugToLabel(Label)
+{
+	const OldDebug = Pop.Debug;
+	const DebugLog = [];
+	const MaxDebugLogLength = 50;
+	Pop.Debug = function()
+	{
+		const DebugString = [...arguments].join(',');
+		DebugLog.splice(0,0,DebugString);
+		DebugLog.splice(MaxDebugLogLength,DebugLog.length);
+		
+		//	update label
+		DebugLabel.SetValue(DebugLog.join('\n'));
+		
+		//	print out stll
+		//OldDebug( ...arguments );
+		OldDebug( DebugString );
+	}
+}
+
+
+//	todo: this app should run even if creating a window fails
 const Window = new Pop.Gui.Window("Pop Asset Server");
 const DebugLabel = new Pop.Gui.Label(Window,[0,0,900,900]);
 DebugLabel.SetValue("Drop folders here");
-
-const OldDebug = Pop.Debug;
-const DebugLog = [];
-const MaxDebugLogLength = 50;
-Pop.Debug = function()
-{
-	const DebugString = [...arguments].join(',');
-	DebugLog.splice(0,0,DebugString);
-	DebugLog.splice(MaxDebugLogLength,DebugLog.length);
-	
-	//	update label
-	Label.SetValue(DebugLog.join('\n'));
-	
-	//	print out stll
-	//OldDebug( ...arguments );
-	OldDebug( DebugString );
-}
-
-const AssetServer_Port = 8088;
-const AssetServer_Directories = ['D:/PopKandinsky/Kandinsky'];
-
-const AssetServer = new Pop.AssetServer(8088);
-
-function OnListenToNewDirectory(Directory)
-{
-	AssetServer.ListenToDirectory(Directory);
-}
-
-AssetServer_Directories.forEach(OnListenToNewDirectory);
-
+RedirectDebugToLabel(DebugLabel);
 
 //	let user drag & drop folders
 async function DragAndDropLoop()
@@ -48,11 +44,62 @@ async function DragAndDropLoop()
 	while (Window)
 	{
 		const Dropped = await Window.WaitForDragDrop();
-		Pop.Debug(JSON.stringify(Dropped));
+		Pop.Debug("Dropped",JSON.stringify(Dropped));
 		Dropped.forEach(OnListenToNewDirectory);
 		await Pop.Yield(100);
 	}
 }
+Pop.Debug("hello");
 DragAndDropLoop().then(Pop.Debug).catch(Pop.Debug);
+
+
+
+//	run server
+const AssetServerPortCount = 10;
+const AssetServer_Ports = [...Array(AssetServerPortCount)].map( (v,i) => 0xF11E + i );	//	generate X ports from F11E (file)
+Pop.Debug(AssetServer_Ports);
+const AssetServer_Directories = [];
+const AssetServer = new Pop.AssetServer(AssetServer_Ports);
+const ListenDirectorySavedFilename = 'ListenDirectories.json';
+
+function OnListenToNewDirectory(Directory)
+{
+	AssetServer_Directories.push(Directory);
+	AssetServer.ListenToDirectory(Directory);
+	Pop.Debug(`Listening for changes in ${Directory}`);
+	
+	try
+	{
+		SaveListenDirectoryList();
+	}
+	catch(e)
+	{
+		Pop.Debug(`Error saving listening directory list ${e}`);
+	}
+}
+
+function SaveListenDirectoryList()
+{
+	const Json = JSON.stringify(AssetServer_Directories,null,'\t');
+	Pop.WriteStringToFile(ListenDirectorySavedFilename,Json);
+}
+
+function LoadListenDirectoryList()
+{
+	try
+	{
+		const Contents = Pop.LoadFileAsString(ListenDirectorySavedFilename);
+		const Filenames = JSON.parse(Contents);
+		Filenames.forEach(OnListenToNewDirectory);
+	}
+	catch(e)
+	{
+		Pop.Debug(`Failed to load directory list ${e}`);
+	}
+}
+
+
+LoadListenDirectoryList();
+
 
 
